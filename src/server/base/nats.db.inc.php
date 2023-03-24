ce07db1001a5ec0d069fd6252080729e8d160e66
David Cutting <dcutting@purplepixie.org>
2023-03-24 11:55:58 -0400

    updated formatting for nats.db

--- src/server/base/nats.db.inc.php
+++ src/server/base/nats.db.inc.php
@@ -27,102 +27,100 @@ function ss($s) // safestring
 }
 
 class TNATS_DB
-	{
-	var $connected=false;
-	var $sql=0;
-	
-	var $LastError=0;
-	var $LastErrorString="";
-	
+{
+	var $connected = false;
+	var $sql = 0;
+
+	var $LastError = 0;
+	var $LastErrorString = "";
+
 	function Connect()
-		{
+	{
 		global $fnCfg;
-		$this->sql=mysqli_connect($fnCfg['db.server'],$fnCfg['db.username'],$fnCfg['db.password'])
+		$this->sql = mysqli_connect($fnCfg['db.server'], $fnCfg['db.username'], $fnCfg['db.password'])
 			or die("Cannot connect to MySQL server");
-		mysqli_select_db($this->sql,$fnCfg['db.database'])
+		mysqli_select_db($this->sql, $fnCfg['db.database'])
 			or die("Cannot select MySQL database");
-		$this->connected=true;
+		$this->connected = true;
 		return $this->sql;
-		}
-		
+	}
+
 	function Disconnect()
-		{
+	{
 		mysqli_close($this->sql);
-		$this->sql=0;
-		$this->connected=false;
-		}
+		$this->sql = 0;
+		$this->connected = false;
+	}
 
 	function SafeString($s)
 	{
-		return mysqli_real_escape_string($this->sql,$s);
+		return mysqli_real_escape_string($this->sql, $s);
 	}
-		
-	function Query($query,$debugerror=true)
-		{
+
+	function Query($query, $debugerror = true)
+	{
 		global $NATS;
-		if (!$this->connected) return -1;
-		$result=mysqli_query($this->sql,$query);
-		if ($debugerror)
-			{
+		if (!$this->connected)
+			return -1;
+		$result = mysqli_query($this->sql, $query);
+		if ($debugerror) {
 			// persist the last error state
-			$this->LastError=mysqli_errno($this->sql);
-			if ($this->LastError>0)
-				{
-				$this->ErrorString=mysqli_error($this->sql)." (".mysqli_errno($this->sql).")";
-				}
-			else $this->ErrorString="";
+			$this->LastError = mysqli_errno($this->sql);
+			if ($this->LastError > 0) {
+				$this->ErrorString = mysqli_error($this->sql) . " (" . mysqli_errno($this->sql) . ")";
+			} else
+				$this->ErrorString = "";
+		}
+
+		if (mysqli_errno($this->sql) > 0) {
+			$err = mysqli_error($this->sql) . " (" . mysqli_errno($this->sql) . ")";
+			if (isset($NATS) && $debugerror) {
+				$NATS->Event("Query Failed: " . $query, 2, "DB", "Query");
+				$NATS->Event("Query Error: " . $err, 2, "DB", "Query");
 			}
-			
-		if (mysqli_errno($this->sql)>0)
-			{
-			$err=mysqli_error($this->sql)." (".mysqli_errno($this->sql).")";
-			if (isset($NATS)&&$debugerror)
-				{
-				$NATS->Event("Query Failed: ".$query,2,"DB","Query");
-				$NATS->Event("Query Error: ".$err,2,"DB","Query");
-				}
-			}
+		}
 		return $result;
-		}
-		
-	function Free(&$result)
-		{
-		mysqli_free_result($result);
-		}
-		
-	function Fetch_Array(&$result)
-		{
-		return mysqli_fetch_array($result);
-		}
-		
-	function Affected_Rows()
-		{
-		return mysqli_affected_rows($this->sql);
-		}
-		
-	function Insert_Id()
-		{
-		return mysqli_insert_id($this->sql);
-		}
-		
-	function Num_Rows(&$result)
-		{
-		return mysqli_num_rows($result);
-		}
-		
-	function Error()
-		{
-		if ($this->LastError==0) return false;
-		return true;
-		}
-		
-	function Error_Number()
-		{
-		return $this->LastError;
-		}
-		
-	function Error_String()
-		{
-		return $this->ErrorString;
-		}
 	}
+
+	function Free(&$result)
+	{
+		mysqli_free_result($result);
+	}
+
+	function Fetch_Array(&$result)
+	{
+		return mysqli_fetch_array($result);
+	}
+
+	function Affected_Rows()
+	{
+		return mysqli_affected_rows($this->sql);
+	}
+
+	function Insert_Id()
+	{
+		return mysqli_insert_id($this->sql);
+	}
+
+	function Num_Rows(&$result)
+	{
+		return mysqli_num_rows($result);
+	}
+
+	function Error()
+	{
+		if ($this->LastError == 0)
+			return false;
+		return true;
+	}
+
+	function Error_Number()
+	{
+		return $this->LastError;
+	}
+
+	function Error_String()
+	{
+		return $this->ErrorString;
+	}
+}
