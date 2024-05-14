8d2d77fb93940cb55476a9a4051da06e8501caca
David Cutting <dcutting@purplepixie.org>
2024-05-14 17:46:32 +0100

    1.30.15

--- sql/latest/schema.sql
+++ sql/latest/schema.sql
@@ -1,4 +1,4 @@
--- FreeNATS freenats-1.30.14a Schema
+-- FreeNATS freenats-1.30.15a Schema
 -- No DROP TABLES - suitable for upgrade
 -- MySQL dump 10.13  Distrib 5.7.34, for osx11.0 (x86_64)
 --
@@ -485,4 +485,4 @@ CREATE TABLE `fnviewitem` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-09 17:52:03
+-- Dump completed on 2024-05-14 17:44:07
--- sql/latest/schema.upgrade.sql
+++ sql/latest/schema.upgrade.sql
@@ -1,4 +1,4 @@
--- FreeNATS freenats-1.30.14a Schema
+-- FreeNATS freenats-1.30.15a Schema
 -- Experimental Upgrade SQL - run after schema.sql (not drop!)
 -- Both will generate many many errors - run with --force, ignore errors
 -- myrug -- PurplePixie Systems
